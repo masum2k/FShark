6d8e3baea23b3d65c10544c0736075f99fdf097e
Sidar Orman <sidarormang@gmail.com>
2025-08-01 13:38:42 +0300

    aa

---
+++ api/Repository/PortfolioRepository.cs
@@ -0,0 +1,32 @@
+﻿using api.Data;
+using api.Interfaces;
+using api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace api.Repository
+{
+    public class PortfolioRepository : IPortfolioRepository
+    {
+        private readonly ApplicationDbContext _applicationDbContext;
+        public PortfolioRepository( ApplicationDbContext applicationDbContext)
+        {
+            _applicationDbContext = applicationDbContext;
+        }
+        public Task<List<Stock>> GetUserPortfolio(AppUser user)
+        {
+            return _applicationDbContext.Portfolios
+                .Where(p => p.AppUserId == user.Id)
+                .Select(p => new Stock
+                {
+                    Id = p.StockId,
+                    Symbol = p.Stock.Symbol,
+                    CompanyName = p.Stock.CompanyName,
+                    Purchase = p.Stock.Purchase,
+                    LastDiv = p.Stock.LastDiv,
+                    Industry = p.Stock.Industry,
+                    MarketCap = p.Stock.MarketCap,
+
+                }).ToListAsync();
+        }
+    }
+}
